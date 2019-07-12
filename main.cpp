faec09f6ea9f624b125c014cc576ebd546b44401
Chris E <chris.earnshaw@york.ac.uk>
2019-07-12 10:57:24 +0100

    added some comments

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main()
 {
 	std::cout << "Greetings, World!\n";
 	
+	// create a list of numbers
 	std::vector<int> newlist = { 1, 2, 3, 4, 5 };
 	
+	// print out the list of numbers on one line
 	for(auto& e : newlist)
 		std::cout << e << " ";
 	
